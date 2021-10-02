cdb0e91fecfa158a5b7f1cdea6d7830740bca1cb
rizialdi <rasselio.diack@gmail.com>
2021-10-02 14:40:37 +0200

    Updated jwt strategies

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,6 +1,6 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 
-import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { UsersService } from 'src/users/users.service';
 import { jwtConstants } from '../constants';
@@ -18,6 +18,9 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   async validate(payload: any) {
     const user = await this.usersService.findOne(payload.sub);
 
+    if (!user) {
+      throw new ForbiddenException();
+    }
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { password, ...userRest } = user;
 
